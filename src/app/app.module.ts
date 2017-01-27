8c9b0c68eb2ba27d4250c32cf80a8a0ca060432c
bujjibabu <bujji404@gmail.com>
2017-01-27 13:00:30 +0530

    initial project setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { CradsComponent } from './crads/crads.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CradsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    MaterialModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/crads/crads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crads',
+  templateUrl: './crads.component.html',
+  styleUrls: ['./crads.component.css']
+})
+export class CradsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
