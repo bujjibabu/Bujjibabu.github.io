1e6f431925e234f09dc58101f5ed94b21c3280ff
bujjibabu <bujji404@gmail.com>
2017-01-28 11:30:51 +0530

    integrated card game.

--- src/app/crads/crads.component.ts
+++ src/app/crads/crads.component.ts
@@ -10,8 +10,32 @@ import {CardService} from '../services/card.service';
   styleUrls: ['./crads.component.scss']
 })
 export class CradsComponent implements OnInit {
+	private cardsData : any;
+	private cardId : any;
+  private showOtherCard : any;
+	private startGame : any;
 
   constructor(public _myService: CardService){};
+
+  highCard() {
+  	this.showOtherCard = true;
+  	if(parseInt(this.cardsData[0].value) < parseInt(this.cardsData[1].value)) {
+  		alert('You Win!')
+    } else {
+    	alert('Better Luck Next Time')
+    }
+    this.startGame = true;
+  };
+
+  lowCard() {
+  	this.showOtherCard = true;
+  	if(parseInt(this.cardsData[0].value) > parseInt(this.cardsData[1].value)) {
+  		alert('You Win!')
+    } else {
+    	alert('Better Luck Next Time')
+    }
+    this.startGame = true;
+  };
 
   //get card id
   getCardId () {
@@ -19,6 +43,7 @@ export class CradsComponent implements OnInit {
     .subscribe(
       data => {
       	console.log(data);
+      	this.cardId = data;
       	this.getCards(data);
       },
       err => {
@@ -31,14 +56,40 @@ export class CradsComponent implements OnInit {
     this._myService.getCards(id)
     .subscribe(
       data => {
-      	console.log(data);
-      	let cards = data;
+      	for (var i = 0; i < data.length; i++) {
+      		switch(data[i].value) {
+				    case 'ACE':
+				        data[i].value = 14;
+				        break;
+				    case 'KING':
+				        data[i].value = 13;
+				        break;
+				    case 'QUEEN':
+				        data[i].value = 12;
+				        break;
+				    case 'JACK':
+				        data[i].value = 11;
+				        break;
+					}
+      	}
+
+      	if(data[0].value === data[1].value) {
+      		this.getCards(this.cardId);
+      	} else {
+      		this.cardsData = data;
+      		console.log('cards::',this.cardsData);
+      	}
       },
       err => {
       console.log(err);
     });
   }
 
+  tryAgain() {
+    this.startGame = false;
+    this.showOtherCard = false;
+    this.getCardId();
+  }
   /**
    * Init method
    */
